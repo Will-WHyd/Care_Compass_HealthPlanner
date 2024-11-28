c65020a42ac2bd03fdee200dd69ff52e823ea32e
William Waldron-Hayden <williamjwh@gmail.com>
2024-11-28 18:19:07 +0000

    set up models for appointments and admin editing

--- med_appt/admin.py
+++ med_appt/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Appointment, Consultant
 
 # Register your models here.
+@admin.register(Appointment)
+class ApptAdmin(admin.ModelAdmin):
+    list_display = ('user', 'procedure', 'appt_date', 'consultant', 'status')
+    list_filter = ['user', 'consultant', 'status']
+
+@admin.register(Consultant)
+class ConsultantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'specialty', 'institution')
+    list_filter = ['name', 'specialty', 'institution']
+
--- med_appt/models.py
+++ med_appt/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
-
-CONFIRMATION = (
+STATUS = (
     (0, 'Pending'),
     (1, 'Attended'), 
     (2, 'Cancelled'),
@@ -11,24 +11,27 @@ CONFIRMATION = (
 
 # Create your models here.
 
+class Consultant(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    specialty = models.CharField(max_length=100, null=False, blank=False, default='General Practitioner')
+    institution = models.CharField(max_length=100, null=False, blank=False, default='No Institution/Hospital set')
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+# Model for creating appointments
 class Appointment(models.Model):
     """
     Stores a single Appointment related to :model:`auth.user`
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     procedure = models.CharField(max_length=200, unique=True)
-    appt_date = models.DateField(default=datetime.date.today)
+    appt_date = models.DateField(default=datetime.now().date())
     appt_time = models.TimeField(auto_now=False, auto_now_add=False)
     address = models.TextField(max_length=200, null=True, blank=True)
     consultant = models.ForeignKey(Consultant, null=False, blank=False, on_delete=models.CASCADE)
-    status = models.CharField(choices=CONFIRMATION, default=0, max_length=50)
+    status = models.IntegerField(choices=STATUS, default=0)
     details = models.TextField(null=True, blank=True)
     followup_details = models.TextField(null=True, blank=True)
     travel_details = models.TextField(null=True, blank=True)
-
-
-class Consultant(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
-
-    def __str__(self):
-        return self.name
