b9a0dd091eac53e75a88f55dca67350bfe94548c
William Waldron-Hayden <williamjwh@gmail.com>
2024-12-12 00:00:48 +0000

    html updates

--- care_compass/settings.py
+++ care_compass/settings.py
@@ -28,6 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+# SECURITY WARNING: Cloudinary configuration
+CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -27,3 +27,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
     });
+
+    setTimeout(function() {
+        let messages = document.getElementById("msg");
+        let alert = new bootstrap.Alert(messages);
+        alert.close();
+    }, 3000);
